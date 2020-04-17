c3b206e013b42e94e20c02bb1b6facf1421fa8d3
Soobin Lee <u6682215@anu.edu.au>
2020-04-17 18:42:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/eyegazeui/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.eyegazeui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ItemAdapter.OnItemListener{
+
+    private  static final String TAG = "MainActivity";
+
+    ItemAdapter itemAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> itemList = new ArrayList<>();
+        for(int i=0; i<3; i++){
+            itemList.add("element"+(i+1)+"");
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        ItemAdapter itemAdapter = new ItemAdapter(itemList,MainActivity.this, this);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this,3);
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        this.itemAdapter = itemAdapter;
+
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        System.out.println("clicked@@@@");
+        Log.d(TAG, "onItemClick: clicked.");
+
+        Intent intent = new Intent(this, itemAdapter.activities[position]);
+
+        startActivity(intent);
+    }
+}
